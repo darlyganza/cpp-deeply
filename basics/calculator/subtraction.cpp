b9ebb60b926b460326a6b6e3c7bad0078c60fbba
darlyganza <umumararunguganzadarlene@gmail.com>
2024-10-29 10:02:56 +0100

    Added simple operations between two variables

---
+++ basics/calculator/subtraction.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int num1, num2, difference;
+	cout<<"Enter first number: ";
+	cin>>num1;
+	cout<< "Enter the second number: ";
+	cin >> num2;
+	difference = num1-num2;
+	cout<<"The difference between "<< num1<<" and "<< num2<<" is: " <<difference<< endl;
+	return 0;
+}
