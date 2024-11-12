c8bf119765c8bb784610f887835241c626b550d7
darlyganza <umumararunguganzadarlene@gmail.com>
2024-11-12 08:19:50 +0100

    Added guessGame.cpp

---
+++ basics/guessGame/guessGame.cpp
@@ -0,0 +1,10 @@
+//This game will involve asking the user what kind(genre) of game they are to play
+//The game will choose a secret(hidden) word
+#include <iostream>
+#include <string>
+
+//includes an exit and different word categories: actor, cities...
+//The user can replay the game
+
+
+
