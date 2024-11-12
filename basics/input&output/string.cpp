9700502aa9d221a0c5e9ba806137a8b84729d115
darlyganza <umumararunguganzadarlene@gmail.com>
2024-11-12 08:50:41 +0100

    updated string.cpp

--- basics/input&output/string.cpp
+++ basics/input&output/string.cpp
@@ -7,9 +7,24 @@ int main(){
 	cout<< firstName << " is my first name and "<< lastName<< " is my last name"<<endl;
 	//insert the letter index number in [] to print out the letter value: 
 	cout<< firstName[0]<< " is the first letter of my first name"<<endl;
-	cout<< lastName.length()<< " letters are in my last name\n";
+	cout<< lastName.length()<< " letters are in my last name"<<endl;
 	//altering the contents(letters) in my firstName use the same approaches of indexes;
 	lastName[2]= 'd';
-	cout<<"my new last name is: "+lastName;
+	cout<<"my new last name is: \n"+lastName <<endl;
+	
+	//string concatenation
+	//1. performed by using the + operator
+	cout<<"Demonstration of string concetanation consider: "<<endl;
+	string name1 = "GANZA";
+	string name2= " Darlene";
+	string fullName = name1+name2;
+	cout <<endl<< "My full name is: "<<fullName<<endl;
+//	2. using the strcat(string1, string2)
+
+//getting user input strings We can use >> with cin:
+string userLastName;
+cout<<"Enter your name (last name) "<<endl;
+cin >> userLastName;
+cout <<"Hello "<<userLastName<<"; How are you doing?\n ";
 	return 0;
 }
