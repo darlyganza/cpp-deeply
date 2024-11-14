d10ac08c33be466cf6b4e2a0f0f0ce04f04b0a68
darlyganza <umumararunguganzadarlene@gmail.com>
2024-11-14 10:05:35 +0100

    Added constants.cpp

--- basics/input&output/namespace.cpp
+++ basics/input&output/namespace.cpp
@@ -6,12 +6,14 @@ namespace people{
 namespace person{
 	string name="Darlene GANZA";
 }
+
 string name="Darly";
 int main(){
 //namespace allows to create scopes for identifiers in a certain scope as such it would be possible for them to have the same names;
 double age;
 
 cout<<people::name;
+//:: the scope operator for accessing elements in the namespace
 cout<<person::name;
 	cout<<"Hello ";
 	return 0;
