d13c1b4889ce350cc58994068b2524ae5e7be47d
darlyganza <umumararunguganzadarlene@gmail.com>
2024-11-17 16:49:47 +0100

    modified helloworld.cpp

--- basics/helloworld/helloworld.cpp
+++ basics/helloworld/helloworld.cpp
@@ -1,7 +1,7 @@
 #include <iostream> //including the header files for the processor directives
 using namespace std; //declaration of variables, and functions as globally available
 int main(){
-	cout <<"Hello world\n\n"; //cout that of the iostream that embedded with stream insertion operators
+	cout <<"Hello world\n"; //cout that of the iostream that embedded with stream insertion operators
 //	\n\n for creating a blank line
 // \n generates words to the new line.
 	cout <<"This is cpp";
