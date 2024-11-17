09044b8fd522fd3af37bf7d7a76696413b86893a
darlyganza <umumararunguganzadarlene@gmail.com>
2024-11-17 17:02:46 +0100

    modified cppConstants.cpp (describing more about cpp constants)

---
+++ basics/constants/cppConstants.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#define myName "Darlene";
+const int age=12;
+using namespace std;
+//cpp constants: These can be literal or symbolic constants:
+//symbolic constants are defined using the const or the define directive: 
+
+int main(){
+	cout<<myName;
+	const int age = 13;
+	cout<<"\nAge: "<<age<<" years";
+	return 0;
+}
