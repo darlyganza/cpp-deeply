b0cd6f2d8a346d5b7ab2f248fcc35b9e3f05f7c0
darlyganza <umumararunguganzadarlene@gmail.com>
2024-11-28 19:46:46 +0100

    updated funcitions.cpp

--- basics/procedural programming/functions/functions.cpp
+++ basics/procedural programming/functions/functions.cpp
@@ -1,9 +1,17 @@
 //program implementing cpp functions: independent but smaller components in cpp programs:
 #include <iostream>
-#include <Math.h>
+#include <cmath>
 using namespace std;
 int main(){
 	int myNum = 16;
-	cout<<sqrt(myNum);
+	double angle =60;
+	cout<<sqrt(myNum)<<"\n";
+	cout<<floor(12.87)<<"\n";
+	cout<<ceil(12.87);
+//printing the maximum value in a certain input of values:
+double maxVal = fmax(12,8);
+cout<< "The maximum value is: "<<maxVal;
+double minVal = fmin(12,8);
+cout <<"\nWhile the minimum value is: "<<minVal;
 	return 0;
 }
