d89c7f41ee7562ab4f603995819bec75f8ad7eb9
darlyganza <umumararunguganzadarlene@gmail.com>
2024-11-28 19:22:19 +0100

    added functions.cpp and modified mergeSort.cpp

---
+++ basics/procedural programming/functions/functions.cpp
@@ -0,0 +1,9 @@
+//program implementing cpp functions: independent but smaller components in cpp programs:
+#include <iostream>
+#include <Math.h>
+using namespace std;
+int main(){
+	int myNum = 16;
+	cout<<sqrt(myNum);
+	return 0;
+}
