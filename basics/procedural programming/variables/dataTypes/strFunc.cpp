a554ce17c6e9a70cfdb38036eb001144c61b541d
darlyganza <umumararunguganzadarlene@gmail.com>
2024-12-07 22:03:23 +0100

    --allow-empty-message

---
+++ basics/procedural programming/variables/dataTypes/strFunc.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string.h>
+
+using namespace std;
+
+int main(){
+int charLen = strlen("Hello nice to be here today");
+//using the strlen to see the number of characters contained:
+cout<<"The characters in the sentence are: "<<charLen;
+
+return 0;
+}
