1ac104d250402b6663dffaa53683f1bb5f58c831
darlyganza <umumararunguganzadarlene@gmail.com>
2024-12-08 20:48:03 +0100

    --allow-empty-message

--- basics/procedural programming/variables/dataTypes/strFunc.cpp
+++ basics/procedural programming/variables/dataTypes/strFunc.cpp
@@ -7,10 +7,6 @@ int charLen = strlen("Hello nice to be here today");
 //using the strlen to see the number of characters contained:
 cout<<"The characters in the sentence are: "<<charLen;
 
-//string manipulation functions: These functions include: 
-string str1 = "Hello ";
-string str2 = " World";
-cout<<"The string 1 and string 2 concantenated using the strcat: "<<strlen(str1.c_str());
 
 return 0;
 }
