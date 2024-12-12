322979327adc475d8149eab8373080c59e618f13
darlyganza <umumararunguganzadarlene@gmail.com>
2024-12-12 05:20:11 +0100

    implemented the initial function of the linearsearch

---
+++ basics/procedural programming/searching/linearsearch.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int impLinearSearch(int arr[],int value, int len){
+int i=0,pos;
+for (i;i<len;i++){
+
+	if(arr[i]==value){
+		pos = i;
+	}
+}
+return pos;
+}
+int main(){
+	int numArr[]={12,3,5,6,12,4,23,54,89,12};
+	int val =4;
+	int len = sizeof(numArr)/sizeof(numArr[0]);
+	cout<<impLinearSearch(numArr,val,len);
+	return 0;
+}
